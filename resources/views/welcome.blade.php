f5ab3fefda0d3334acb0113c10509b4489b3a8bd
kamaltz <kamaltz.dena16@gmail.com>
2025-06-29 20:46:10 +0700

    header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Reading Hub - Platform Pemahaman Membaca Generasi Baru</title>
+    <title>Reading Hub</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700,800&display=swap" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
